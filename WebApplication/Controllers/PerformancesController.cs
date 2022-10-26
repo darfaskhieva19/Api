c47b0637464110cd89380a632d4e7a649f7b44a6
ФасхиеваДР <@105-11>
2022-10-26 14:36:17 +0300

    Добавьте файлы проекта.

---
+++ WebApplication/Controllers/PerformancesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class PerformancesController : ApiController
+    {
+        private PerfomEntities db = new PerfomEntities();
+
+        // GET: api/Performances
+        [ResponseType(typeof(List<ModelPerfomance>))]
+        public IHttpActionResult GetPerformances()
+        {
+            return Ok(db.Performances.ToList().ConvertAll(x => new ModelPerfomance(x)));
+        }
+
+        // GET: api/Performances/5
+        [ResponseType(typeof(Performances))]
+        public IHttpActionResult GetPerformances(int id)
+        {
+            Performances performances = db.Performances.Find(id);
+            if (performances == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(performances);
+        }
+
+        // PUT: api/Performances/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPerformances(int id, Performances performances)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != performances.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(performances).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PerformancesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Performances
+        [ResponseType(typeof(Performances))]
+        public IHttpActionResult PostPerformances(Performances performances)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Performances.Add(performances);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = performances.ID }, performances);
+        }
+
+        // DELETE: api/Performances/5
+        [ResponseType(typeof(Performances))]
+        public IHttpActionResult DeletePerformances(int id)
+        {
+            Performances performances = db.Performances.Find(id);
+            if (performances == null)
+            {
+                return NotFound();
+            }
+
+            db.Performances.Remove(performances);
+            db.SaveChanges();
+
+            return Ok(performances);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PerformancesExists(int id)
+        {
+            return db.Performances.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ WebApplication/Models/ModelPerfomance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class ModelPerfomance
+    {
+        public ModelPerfomance(Performances performance)
+        {
+            ID = performance.ID;
+            Title = performance.Title;
+            Genre = performance.Genre;
+            Producer = performance.Producer;
+            Image = performance.Image;  
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Producer { get; set; }
+        public string Image { get; set; }   
+
+    }
+}
